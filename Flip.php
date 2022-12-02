c27d8ce6c4bd115825a03e9f4b61b21010d53fa7
KUMAR PIYUSH <107334940+KumarPiyush89@users.noreply.github.com>
2022-12-02 17:18:00 +0530

    Add files via upload

---
+++ Flip.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css"/>
+    <title>Turnig book</title>
+</head>
+<body>
+    <div class="imgLoader"></div>
+    <div class="container">
+        <div class="book">
+            <div class="gap"></div>
+            <div class="pages">
+                <div class="page"></div>
+                <div class="page"></div>
+                <div class="page"></div>
+                <div class="page"></div>
+                <div class="page"></div>
+                <div class="page"></div>
+            </div>
+        <div class="flips">
+            <div class="flip flip1">
+            <div class="flip flip2">
+            <div class="flip flip3">
+            <div class="flip flip4">
+            <div class="flip flip5">
+            <div class="flip flip6">
+            <div class="flip flip7"></div>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    
+</body>
+</html>
